c8c31f7373123e6d93ccda4610991430eb4f21d6
apolloandroid <hmelevskih94@gmail.com>
2020-07-25 18:24:35 +0300

    Added example of FlyWeight patterng

---
+++ Structural/FlyWeight/src/trees/TreeType.java
@@ -0,0 +1,22 @@
+package trees;
+
+import java.awt.*;
+
+public class TreeType {
+    public String name;
+    public Color color;
+    public String otherTreeData;
+
+    public TreeType(String name, Color color, String otherTreeData) {
+        this.name = name;
+        this.color = color;
+        this.otherTreeData = otherTreeData;
+    }
+
+    public void draw(Graphics g, int x, int y) {
+        g.getColor();
+        g.fillRect(x - 1, y, 3, 5);
+        g.setColor(color);
+        g.fillOval(x - 5, y - 10, 10, 10);
+    }
+}
