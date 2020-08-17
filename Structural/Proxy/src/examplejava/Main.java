248cab336da2967d5cc0d1547ec89a7a2772cc70
apolloandroid <hmelevskih94@gmail.com>
2020-08-17 22:35:34 +0300

    Example of Proxy pattern in Java

--- Structural/Proxy/src/examplejava/Main.java
+++ Structural/Proxy/src/examplejava/Main.java
@@ -1,19 +1,20 @@
 package examplejava;
 
+import examplejava.medialibrary.Downloader;
 import examplejava.medialibrary.YouTubeCacheProxy;
 import examplejava.medialibrary.YouTubeClass;
 
 public class Main {
     public static void main(String[] args) {
-        YouTubeDownloader naiveDownloader = new YouTubeDownloader(new YouTubeClass());
-        YouTubeDownloader smartDownloader = new YouTubeDownloader(new YouTubeCacheProxy());
+        Downloader naiveDownloader = new Downloader(new YouTubeClass());
+        Downloader smartDownloader = new Downloader(new YouTubeCacheProxy());
 
         long naive = test(naiveDownloader);
         long smart = test(smartDownloader);
         System.out.println("Time saved by caching proxy: " + (naive - smart) + "ms");
     }
 
-    private static long test(YouTubeDownloader downloader) {
+    private static long test(Downloader downloader) {
         long startTime = System.currentTimeMillis();
 
         downloader.renderPopularVideos();
