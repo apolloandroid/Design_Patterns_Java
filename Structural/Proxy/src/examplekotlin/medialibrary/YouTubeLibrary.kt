a068988d7e0750625d0d1de976f970438fabc237
apolloandroid <hmelevskih94@gmail.com>
2020-08-18 22:32:24 +0300

    Example of Proxy pattern in Kotlin

--- Structural/Proxy/src/examplekotlin/medialibrary/YouTubeLibrary.kt
+++ Structural/Proxy/src/examplekotlin/medialibrary/YouTubeLibrary.kt
@@ -3,6 +3,6 @@ package examplekotlin.medialibrary
 import java.util.HashMap
 
 interface YouTubeLibrary {
-    fun popularVideos(): HashMap<String, Video>?
+    fun popularVideos(): HashMap<String, Video>
     fun getVideo(videoId: String): Video
 }
