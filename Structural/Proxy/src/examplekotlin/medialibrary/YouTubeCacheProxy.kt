44acb067010ba3fe419427af56fa6fc738cfab5a
apolloandroid <hmelevskih94@gmail.com>
2020-08-18 22:31:44 +0300

    Example of Proxy pattern in Kotlin

--- Structural/Proxy/src/examplekotlin/medialibrary/YouTubeCacheProxy.kt
+++ Structural/Proxy/src/examplekotlin/medialibrary/YouTubeCacheProxy.kt
@@ -1,4 +1,32 @@
 package examplekotlin.medialibrary
 
-class YouTubeCacheProxy {
+import java.util.*
+
+class YouTubeCacheProxy : YouTubeLibrary {
+    private val youTubeLibrary: YouTubeLibrary = YouTubeClass()
+    private var cachePopularVideos = HashMap<String, Video>()
+    private val cacheAll = HashMap<String, Video>()
+
+    override fun popularVideos(): HashMap<String, Video>{
+        if (cachePopularVideos.isEmpty()){
+            cachePopularVideos = youTubeLibrary.popularVideos()!!
+        }
+        else    {
+            println("Retrieved list from cache")
+        }
+        return cachePopularVideos
+    }
+
+    override fun getVideo(videoId: String): Video {
+        var video: Video? = cacheAll[videoId]
+        if (video == null) {
+            video = youTubeLibrary.getVideo(videoId)
+            cacheAll[videoId] = video
+        } else {
+            println("Retrieved video '$videoId' from cache.")
+        }
+        return video
+    }
+
+
 }
