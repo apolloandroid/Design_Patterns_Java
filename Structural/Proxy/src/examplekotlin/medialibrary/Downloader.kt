a082228ec7d5996c511489d6dae3dee1822776f9
apolloandroid <hmelevskih94@gmail.com>
2020-08-18 22:31:27 +0300

    Example of Proxy pattern in Kotlin

--- Structural/Proxy/src/examplekotlin/medialibrary/Downloader.kt
+++ Structural/Proxy/src/examplekotlin/medialibrary/Downloader.kt
@@ -1,2 +1,24 @@
 package examplekotlin.medialibrary
 
+
+class Downloader(private val api: YouTubeLibrary) {
+    fun renderVideoPage(videoId: String?) {
+        val video = videoId?.let { api.getVideo(it) }
+        println("\n-------------------------------")
+        println("Video page (imagine fancy HTML)")
+        println("ID: " + video?.id)
+        println("Title: " + video?.title)
+        println("Video: " + video?.data)
+        println("-------------------------------\n")
+    }
+
+    fun renderPopularVideos() {
+        val list = api.popularVideos()
+        println("\n-------------------------------")
+        println("Most popular videos on YouTube (imagine fancy HTML)")
+        for (video in list.values) {
+            println("ID: " + video.id + " / Title: " + video.title)
+        }
+        println("-------------------------------\n")
+    }
+}
