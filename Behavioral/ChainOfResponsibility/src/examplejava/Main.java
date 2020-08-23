e3dcbf21bac468373f12d180fa31845226894f33
apolloandroid <hmelevskih94@gmail.com>
2020-08-23 18:01:49 +0300

    Example of Chain of responsibility pattern in Java

---
+++ Behavioral/ChainOfResponsibility/src/examplejava/Main.java
@@ -0,0 +1,4 @@
+package examplejava;
+
+public class Main {
+}
---
+++ Behavioral/ChainOfResponsibility/src/examplejava/server/Server.java
@@ -0,0 +1,13 @@
+package examplejava.server;
+
+public class Server {
+
+
+    public boolean hasEmail(String email) {
+        return  true;
+    }
+
+    public boolean isValid(String email, String password) {
+        return  true;
+    }
+}
