e9fc5eb2d7d97a1e12968f28009c95e0e779a957
apolloandroid <hmelevskih94@gmail.com>
2020-08-29 19:56:12 +0300

    Example of Proxy pattern in Kotlin

--- Behavioral/ChainOfResponsibility/src/examplejava/Main.java
+++ Behavioral/ChainOfResponsibility/src/examplejava/Main.java
@@ -1,4 +1,38 @@
 package examplejava;
 
+import examplejava.middleware.Middleware;
+import examplejava.middleware.RoleCheckMiddleware;
+import examplejava.middleware.ThrottlingMiddleware;
+import examplejava.middleware.UserExistsMiddleware;
+import examplejava.server.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    private static Server server;
+
+    public static void main(String[] args) throws IOException {
+        boolean success;
+        do {
+            System.out.print("Enter email: ");
+            String email = reader.readLine();
+            System.out.print("Enter password: ");
+            String password = reader.readLine();
+            success = server.logIn(email, password);
+        } while (!success);
+    }
+
+    private static void init() {
+        server = new Server();
+        server.register("admin@example.com", "admin_pass");
+        server.register("user@example.com", "user_pass");
+        Middleware middleware = new ThrottlingMiddleware(2);
+        middleware.linkWith(new UserExistsMiddleware(server))
+                .linkWith(new RoleCheckMiddleware());
+        server.setMiddleware(middleware);
+
+    }
 }
--- Behavioral/ChainOfResponsibility/src/examplejava/server/Server.java
+++ Behavioral/ChainOfResponsibility/src/examplejava/server/Server.java
@@ -1,13 +1,35 @@
 package examplejava.server;
 
-public class Server {
+import examplejava.middleware.Middleware;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class Server {
+    private Map<String, String> users = new HashMap<>();
+    private Middleware middleware;
+
+    public void setMiddleware(Middleware middleware) {
+        this.middleware = middleware;
+    }
+
+    public boolean logIn(String email, String password) {
+        if (middleware.check(email, password)) {
+            System.out.println("Authorization have been successful!");
+            return true;
+        }
+        return false;
+    }
+
+    public void register(String email, String password) {
+        users.put(email, password);
+    }
 
     public boolean hasEmail(String email) {
-        return  true;
+        return true;
     }
 
     public boolean isValid(String email, String password) {
-        return  true;
+        return true;
     }
 }
