511ea1a91b9546dece77ef658357282c626162b1
apolloandroid <hmelevskih94@gmail.com>
2020-09-20 18:30:56 +0300

    Example of Chain of responsibility in Kotlin

--- Behavioral/ChainOfResponsibility/src/examplekotlin/middleware/Middleware.kt
+++ Behavioral/ChainOfResponsibility/src/examplekotlin/middleware/Middleware.kt
@@ -1,4 +1,15 @@
 package examplekotlin.middleware
 
 abstract class Middleware {
+    private lateinit var next: Middleware
+    fun linkWith(next: Middleware): Middleware {
+        this.next = next
+        return next
+    }
+
+    abstract fun check(email: String, password: String): Boolean
+
+    protected open fun checkNext(email: String, password: String): Boolean {
+        return if (next == null) true else next.check(email, password)
+    }
 }
--- Behavioral/ChainOfResponsibility/src/examplekotlin/server/Server.kt
+++ Behavioral/ChainOfResponsibility/src/examplekotlin/server/Server.kt
@@ -4,7 +4,21 @@ import examplejava.middleware.Middleware
 
 class Server {
     private val users = HashMap<String, String>()
-    private lateinit var middleware: Middleware
+    lateinit var middleware: Middleware
 
+    fun logIn(email: String, password: String): Boolean {
+        if (middleware!!.check(email, password)) {
+            println("Authorization have been successful!")
+            return true
+        }
+        return false
+    }
 
+    fun register(email: String, password: String) {
+        users[email] = password
+    }
+
+    fun hasEmail(email: String) = users.containsKey(email)
+
+    fun isValid(email: String?, password: String) = users[email] == password
 }
